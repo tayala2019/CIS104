a818430134c318986f9801fc4c4f491aec89ed6d
Ayala.Tamishia <tamishia.ayala@ad.igt.com>
2019-02-19 20:13:42 -0800

    commit

--- calculator.py
+++ calculator.py
@@ -1,7 +1,9 @@
 import math
+
 #Funtion to sum two numbers
 def add(x,y):
     return x + y
+    
 #Funtion to find the difference of two numbers
 def sub(x,y):
         return x - y
@@ -16,8 +18,11 @@ def square(x):
 #Funtion to find the square root 
     return math.sqrt(x)
 def power(x,y): 
-#Funtion to find the power of a number 
-    return (pow(x, y))
-    
-def clearMemory():
-    return print (0)
+ return (pow(x, y))
+memoryVal=0
+#Clears memory value
+def clearMem():
+   memoryVal=0
+
+
+
--- main.py
+++ main.py
@@ -6,43 +6,52 @@ import os
 
 repeat = True
 while repeat:
-    print ("What calculation would you like to perform: ")
+    print ("\nWhat calculation would you like to perform: \n")
     print ("1: Addition")
     print ("2: Subtraction")
     print ("3: Multiplication")
     print ("4: Division")
     print ("5: Srqt")
     print ("6: Power")
-    print ("7: Quit")   
-       
-    #Capture the menu choice.
+    print ("7: Clear Value")   
+    print('\n')  
     choice = input()   
+    
              
     if choice == "1":
         Num1 = int(input("Enter your first number: "))
         Num2 = int(input("Enter your second number: "))
-        print ('Your result is:'+ str(calculator.add(Num1,Num2)))
+        calculator.memoryVal = calculator.add(Num1,Num2)
+        print ('Your result is:'+ str(calculator.memoryVal))
+        
     elif choice == "2":
         Num1 = int(input("Enter your first number: "))
         Num2 = int(input("Enter your second number: "))
-        print ('Your result is:'+ str(calculator.sub(Num1,Num2)))
+        calculator.memoryVal = calculator.sub(Num1,Num2)
+        print ('Your result is:'+ str(calculator.memoryVal))
+        calculator.memoryVal = calculator.inver(Num1,Num2)
+        print ('Your result is:'+ str(calculator.memoryVal))
     elif choice == "3":
         Num1 = int(input("Enter your first number: "))
         Num2 = int(input("Enter your second number: "))
-        print ('Your result is:'+ str(calculator.multi(Num1,Num2)))
+        calculator.memoryVal = calculator.multi(Num1,Num2)
+        print ('Your result is:'+ str(calculator.memoryVal))
     elif choice == "4":
         Num1 = int(input("Enter your first number: "))
         Num2 = int(input("Enter your second number: "))
-        print ('Your result is:'+ str(calculator.div(Num1,Num2)))
+        calculator.memoryVal = calculator.div(Num1,Num2)
+        print ('Your result is:'+ str(calculator.memoryVal))
     elif choice == "5":
         Num1 = int(input("Enter your first number: "))
-        print ('Your result is:'+ str(calculator.square(Num1)))
+        calculator.memoryVal = calculator.square(Num1)
+        print ('Your result is:'+ str(calculator.memoryVal))
     elif choice == "6":
         Num1 = int(input("Enter your first number: "))
         Num2 = int(input("Enter raised to the power of: "))
-        print ('Your result is:'+ str(calculator.power(Num1,Num2)))
+        calculator.memoryVal = calculator.power(Num1,Num2)
+        print ('Your result is:'+ str(calculator.memoryVal))
     elif choice == "7":
-        print ('Your result is:'+ str(calculator.clearMemory()))
+        print ('Your result is:'+ str(calculator.clearMem()))
 else:
-    print ('Choice is invalid.')
+    print ('Invalid choice.')
    
