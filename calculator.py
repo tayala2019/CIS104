8c42bc42b67152428632bddc7ea072189df531c1
tayala2019 <46700817+tayala2019@users.noreply.github.com>
2019-02-19 17:27:39 -0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,23 @@
+import math
+#Funtion to sum two numbers
+def add(x,y):
+    return x + y
+#Funtion to find the difference of two numbers
+def sub(x,y):
+        return x - y
+#Funtion to multiply  two numbers
+def multi(x,y):
+        return x * y
+#Funtion to divide  two numbers
+def div(x,y):
+        return x / y
+#Funtion to divide  two numbers
+def square(x): 
+#Funtion to find the square root 
+    return math.sqrt(x)
+def power(x,y): 
+#Funtion to find the power of a number 
+    return (pow(x, y))
+    
+def clearMemory():
+    return print (0)
